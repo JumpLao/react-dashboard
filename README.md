ed63ea8f821fa07d2d494f0ef4d242a896fa9759
Nattaphat Laoharawee <nattaphat.la@gmail.com>
2019-12-11 11:07:13 +0700

    add screenshot

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-dashboard
+
+## Screenshots
+![Alt text](/doc/images/signin.png?raw=true "Sign in page")
+![Alt text](/doc/images/signup.png?raw=true "Sign up page")
+![Alt text](/doc/images/listpage.png?raw=true "List page")
--- app/src/pages/Profile/routes.js
+++ app/src/pages/Profile/routes.js
@@ -7,12 +7,10 @@ import Exception from "../Exception";
 const basePath = '/profile'
 const profileRoute = [
   {
-    menu: 'Profile',
     path: basePath,
     component: Root,
     routes: [
       {
-        menu: 'My profile',
         path: basePath,
         exact: true,
         component: Show,
